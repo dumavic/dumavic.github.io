53962a508fca8a429ff7f2259595aa136350c2af
Victor Nguyen <vicvngu@gmail.com>
2025-02-09 22:18:10 -0500

    updated for assignment 07 and the project 03

--- csce242-assignments/05/script.js
+++ csce242-assignments/05/script.js
@@ -3,30 +3,45 @@ const changeImg = () => {
     document.getElementById("changeImg").src = "./images/200black.gif";
 
 };
-const rotateImg = () => {
-    const slider = document.getElementById("myRange");
-    const image = document.getElementById("img-spin");
 
-    // oninput calls function when user uses the input form
-    slider.oninput = function() {
-        const rotationValue = this.value;
-        console.log("Rotation value:", rotationValue);
-        // Apply rotation to the image
-        image.style.transform = "rotate(" + rotationValue + "deg)";
-    };
+// const rotateImg = () => {
+//     const slider = document.getElementById("myRange");
+//     const image = document.getElementById("img-spin");
+
+//     // oninput calls function when user uses the input form
+//     slider.oninput = function() {
+//         const rotationValue = this.value;
+//         console.log("Rotation value:", rotationValue);
+//         // Apply rotation to the image
+//         image.style.transform = "rotate(" + rotationValue + "deg)";
+//     };
+// };
+
+const colorPicker = document.getElementById('color-picker');
+const star = document.getElementById('star');
+
+const changeColor = (event) => {
+  const selectedColor = event.target.value;
+  star.style.color = selectedColor;
 };
 
-const addStar = () => {
+colorPicker.addEventListener('input', changeColor);
 
-    const playground = document.getElementById("star-playground");
-    const star = document.createElement("div");
-    star.classList.add("five-pointed-star");
-    playground.append(star);
+const addHello = () => {
+
+    const playground = document.getElementById("hello-playground");
+    const result = document.getElementById("hello-result");
+    const newP = document.createElement('p');
+    const paragraphContent = document.createTextNode('Hello');
+
+    newP.appendChild(paragraphContent);
+
+    result.appendChild(newP);
 }
 
 
 document.getElementById("changeImg").onclick = changeImg;
-rotateImg();
+// rotateImg();
 
 
-document.getElementById("add-stars").onclick = addStar;
+document.getElementById("hello-playground").onclick = addHello;
